203cc3ad08bf1033798e25fa385a469c613cfb12
Thomas-SanMartin <123770648+Thomas-SanMartin@users.noreply.github.com>
2024-12-23 22:12:38 -0700

    tools component complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
   { path: 'tasks', loadComponent: () => import('./tasks/tasks.component').then(m => m.TasksComponent) },
-  { path: 'news', loadComponent: () => import('./news/news.component').then(m => m.NewsComponent) }
+  { path: 'news', loadComponent: () => import('./news/news.component').then(m => m.NewsComponent) },
+  { path: 'tools', loadComponent: () => import('./tools/tools.component').then(m => m.ToolsComponent) }
 ];
