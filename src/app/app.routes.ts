97db816504f352e95da47ca1af46c9e90cc50c1f
Thomas-SanMartin <123770648+Thomas-SanMartin@users.noreply.github.com>
2024-12-23 21:36:20 -0700

    tasks component complete, home files organized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
+  { path: 'tasks', loadComponent: () => import('./tasks/tasks.component').then(m => m.TasksComponent) }
 ];
